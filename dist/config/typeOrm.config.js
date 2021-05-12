7b60454020d4162626eead27136e20bd9a5c34e5
amarouche <amarou_o@etna-alternance.net>
2021-05-12 20:20:41 +0200

    add prod env

--- dist/config/typeOrm.config.js
+++ dist/config/typeOrm.config.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeOrmConfig = void 0;
 exports.typeOrmConfig = {
     type: 'postgres',
-    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    host: process.env.NODE_ENV == 'production' ? process.env.POSTGRES_HOST_PROD : process.env.POSTGRES_HOST || '127.0.0.1',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAM,
+    username: process.env.NODE_ENV == 'production' ? process.env.DATABASE_USERNAME_PROD : process.env.DATABASE_USERNAME,
+    password: process.env.NODE_ENV == 'production' ? process.env.DATABASE_PASSWORD_PROD : process.env.DATABASE_PASSWORD,
+    database: process.env.NODE_ENV == 'production' ? process.env.DATABASE_NAM_PROD : process.env.DATABASE_NAM,
     entities: ["dist/**/*.entity{.ts,.js}"],
     synchronize: true
 };
