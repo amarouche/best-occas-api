c5df9e49f573df12552eeb570be4b12f7225686d
amarouche <amarou_o@etna-alternance.net>
2021-10-12 20:47:39 +0200

    edit port

--- config/typeOrm.config.ts
+++ config/typeOrm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig : TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.NODE_ENV== 'production' ? process.env.POSTGRES_HOST_PROD : process.env.POSTGRES_HOST || '127.0.0.1',
-  port: process.env.NODE_ENV == 'production' ? parseInt(process.env.PORT) :  parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
   username: process.env.NODE_ENV == 'production' ? process.env.DATABASE_USERNAME_PROD : process.env.DATABASE_USERNAME,
   password: process.env.NODE_ENV == 'production' ? process.env.DATABASE_PASSWORD_PROD : process.env.DATABASE_PASSWORD,
   database: process.env.NODE_ENV == 'production' ? process.env.DATABASE_NAM_PROD : process.env.DATABASE_NAM,
